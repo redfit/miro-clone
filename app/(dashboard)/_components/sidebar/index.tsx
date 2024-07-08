970a461475eed6a93292bc47a8a59ee873068001
redfit <nagita.0724.0812@gmail.com>
2024-07-09 08:39:58 +0900

    sidebar

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,9 +1,12 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { NewButton } from "@/app/(dashboard)/_components/sidebar/new-button";
+import { List } from "@/app/(dashboard)/_components/sidebar/list";
 
 const Sidebar = () => {
   return (
     <aside className="fixed z-[1] left-0 bg-blue-950 h-full w-[60px] flex p-3 flex-col gap-y-4 text-white">
-      <div>Side</div>
+      <List />
+      <NewButton />
     </aside>
   );
 };
