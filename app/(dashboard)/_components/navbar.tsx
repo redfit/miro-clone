8acab510fe0c7367421f33107866675f6f1b7f1c
redfit <nagita.0724.0812@gmail.com>
2024-07-11 11:29:10 +0900

    NavBar

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,12 +1,48 @@
 "use client";
 
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  useOrganization,
+  UserButton,
+} from "@clerk/nextjs";
+import SearchInput from "@/app/(dashboard)/_components/sidebar/search-input";
+import { OrganizationSwitcher } from "@clerk/clerk-react";
+import InviteButton from "@/app/(dashboard)/_components/invite-button";
 
 const NavBar = () => {
+  const { organization } = useOrganization();
+
   return (
     <>
-      <div className="flex items-center gap-x-4 p-5 bg-green-500">
-        <div className="hidden lg:flex lg:flex-1 bg-yellow-500">Search</div>
+      <div className="flex items-center gap-x-4 p-5">
+        <div className="hidden lg:flex lg:flex-1">
+          <SearchInput />
+        </div>
+        <div className="block lg:hidden flex-1">
+          <OrganizationSwitcher
+            hidePersonal
+            appearance={{
+              elements: {
+                rootBox: {
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "100%",
+                },
+                organizationSwitcherTrigger: {
+                  padding: "6px",
+                  width: "100%",
+                  borderRadius: "8px",
+                  border: "1px solid #E5E7EB",
+                },
+              },
+            }}
+          />
+        </div>
+        {organization && <InviteButton />}
+
         <SignedOut>
           <SignInButton />
         </SignedOut>
