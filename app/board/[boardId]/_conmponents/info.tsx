8aae8a33f7f2476d64811c70d479eab41e3e75df
redfit <nagita.0724.0812@gmail.com>
2024-07-15 20:08:03 +0900

    Room info

--- app/board/[boardId]/_conmponents/info.tsx
+++ app/board/[boardId]/_conmponents/info.tsx
@@ -1,14 +1,17 @@
 "use client";
 
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Hint } from "@/components/hint";
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { Poppins } from "next/font/google";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import { cn } from "@/lib/utils";
-import Link from "next/link";
-import { Hint } from "@/components/hint";
+import { useRenameModal } from "@/store/use-rename-modal";
+import { Actions } from "@/components/actions";
+import { Menu } from "lucide-react";
 
 interface InfoProps {
   boardId: string;
@@ -24,6 +27,8 @@ const TabSeparator = () => {
 };
 
 const Info = ({ boardId }: InfoProps) => {
+  const { onOpen } = useRenameModal();
+
   const data = useQuery(api.board.get, {
     id: boardId as Id<"boards">,
   });
@@ -48,7 +53,25 @@ const Info = ({ boardId }: InfoProps) => {
         </Button>
       </Hint>
       <TabSeparator />
-      <Button className="text-basefont-normal px-2">{data.title}</Button>
+      <Hint label="Edit title" side="bottom" sideOffset={10}>
+        <Button
+          onClick={() => onOpen(data._id, data.title)}
+          variant="board"
+          className="text-basefont-normal px-2"
+        >
+          {data.title}
+        </Button>
+      </Hint>
+      <TabSeparator />
+      <Actions id={data._id} title={data.title} side="bottom" sideOffset={10}>
+        <div>
+          <Hint label="Main manu" side="bottom" sideOffset={10}>
+            <Button variant="board" size="icon">
+              <Menu />
+            </Button>
+          </Hint>
+        </div>
+      </Actions>
     </div>
   );
 };
