3eaa7426495378fda29bc26f5e6f76529040846a
redfit <nagita.0724.0812@gmail.com>
2024-07-15 22:08:52 +0900

    canvas state

--- app/board/[boardId]/_conmponents/canvas.tsx
+++ app/board/[boardId]/_conmponents/canvas.tsx
@@ -4,20 +4,41 @@ import Info from "@/app/board/[boardId]/_conmponents/info";
 import Participants from "@/app/board/[boardId]/_conmponents/participants";
 import Toolbar from "@/app/board/[boardId]/_conmponents/toolbar";
 
-import { useSelf } from "@/liveblocks.config";
+import { CanvasMode, CanvasState } from "@/types/canvas";
+
+import {
+  useCanRedo,
+  useHistory,
+  useCanUndo,
+  useSelf,
+} from "@/liveblocks.config";
+import { useState } from "react";
 
 interface CanvasProps {
   boardId: string;
 }
 
 const Canvas = ({ boardId }: CanvasProps) => {
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  });
   const info = useSelf((me) => me.info);
+  const history = useHistory();
+  const canUndo = useCanUndo();
+  const canRedo = useCanRedo();
 
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
       <Participants />
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        canUndo={canUndo}
+        canRedo={canRedo}
+        undo={history.undo}
+        redo={history.redo}
+      />
     </main>
   );
 };
---
+++ types/canvas.ts
@@ -0,0 +1,110 @@
+export type Color = {
+  r: number;
+  g: number;
+  b: number;
+};
+
+export type Camera = {
+  x: number;
+  y: number;
+};
+
+export enum LayerType {
+  Rectangle,
+  Ellipse,
+  Path,
+  Text,
+  Note,
+}
+
+export type BaseLayer = {
+  x: number;
+  y: number;
+  height: number;
+  width: number;
+  fill: Color;
+  value?: string;
+};
+
+export type RectangleLayer = BaseLayer & {
+  type: LayerType.Rectangle;
+};
+
+export type EllipseLayer = BaseLayer & {
+  type: LayerType.Ellipse;
+};
+
+export type PathLayer = BaseLayer & {
+  type: LayerType.Path;
+  points: number[][];
+};
+
+export type TextLayer = BaseLayer & {
+  type: LayerType.Text;
+};
+
+export type NoteLayer = BaseLayer & {
+  type: LayerType.Note;
+};
+
+export type Point = {
+  x: number;
+  y: number;
+};
+
+export type XYWH = Point & {
+  height: number;
+  width: number;
+};
+
+export enum Side {
+  Top,
+  Bottom,
+  Left,
+  Right,
+}
+
+export type CanvasState =
+  | {
+      mode: CanvasMode.None;
+    }
+  | {
+      mode: CanvasMode.SelectionNet;
+      origin: Point;
+      current?: Point;
+    }
+  | {
+      mode: CanvasMode.Translating;
+      current: Point;
+    }
+  | {
+      mode: CanvasMode.Inserting;
+      layerType:
+        | LayerType.Ellipse
+        | LayerType.Rectangle
+        | LayerType.Text
+        | LayerType.Note;
+    }
+  | {
+      mode: CanvasMode.Pencil;
+    }
+  | {
+      mode: CanvasMode.Pressing;
+      origin: Point;
+    }
+  | {
+      mode: CanvasMode.Resizing;
+      initialBounds: XYWH;
+      corner: Side;
+    };
+
+export enum CanvasMode {
+  None,
+  Pressing,
+  SelectionNet,
+  Translating,
+  Inserting,
+  Resizing,
+  Pencil,
+  Note,
+}
